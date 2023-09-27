0423450a16719a5b8ed8f4662c8d5fb791c9762d
dllchun <87022511+dllchun@users.noreply.github.com>
2023-09-27 16:09:49 +0000

    add complete

--- 1_Beginner_Project/to-do/main.py
+++ 1_Beginner_Project/to-do/main.py
@@ -1,20 +1,28 @@
 todos = []
 
 while True:
-    user_action = input("Type add, show, edit or exit:  ").strip()
+    user_action = input("Type add, show, edit, complete or exit:  ").strip()
 
     match user_action:
         case "add":
             todo = input("Enter a todo: ")
             todos.append(todo)
         case "show":
-            for item in todos:
-                print(item)
+            for index, item in enumerate(todos):
+                print(f"{index + 1} -- {item}")
         case "edit":
             item_to_be_edit = input('what would you like to change: ')
             position = todos.index(item_to_be_edit)
             new_item = input("what would you like to change to: ")
             todos[position] = new_item
+            
+        case "complete":
+            item_completed = input('which item did you completed? ')
+            position = todos.index(item_completed)
+            todos.pop(position)
+            
+            
+            
         case 'exit':
             break
 
