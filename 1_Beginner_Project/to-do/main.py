19c81e49b6f6221aaf3b8a3f7ec8ae79fa56e70c
dllchun <87022511+dllchun@users.noreply.github.com>
2023-09-27 14:08:20 +0000

    test codespace

--- 1_Beginner_Project/to-do/main.py
+++ 1_Beginner_Project/to-do/main.py
@@ -1,23 +1,23 @@
 todos = []
 
 while True:
-    user_action = input("Type add, show or exit:  ").strip()
-    
+    user_action = input("Type add, show, edit or exit:  ").strip()
+
     match user_action:
         case "add":
             todo = input("Enter a todo: ")
             todos.append(todo)
-
-            
         case "show":
             for item in todos:
                 print(item)
-            
+        case "edit":
+            item_to_be_edit = input('what would you like to change: ')
+            position = todos.index(item_to_be_edit)
+            new_item = input("what would you like to change to: ")
+            todos[position] = new_item
         case 'exit':
             break
 
         case _:
             print('you have answered an unknown command')
-
 print('bye')
-
