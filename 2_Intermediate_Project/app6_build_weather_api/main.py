3074ada07466b94b053dcbc4f7df217f93e371a0
dllchun <wingchuncheung0108@gmail.com>
2023-10-18 23:13:17 +0800

    day 31 start

--- 2_Intermediate_Project/app6_build_weather_api/main.py
+++ 2_Intermediate_Project/app6_build_weather_api/main.py
@@ -4,35 +4,57 @@ import requests
 import json
 
 # from data import request_weather
-
-
 app = Flask(
     __name__,
     template_folder="/Users/vincentcheung/Desktop/Coding/Python-1/2_Intermediate_Project/app6_build_weather_api/templates",
     static_folder="/Users/vincentcheung/Desktop/Coding/Python-1/2_Intermediate_Project/app6_build_weather_api/static",
 )
 
+station_header = ["id", "station_name", "cn", "lat", "lon", "hght"]
+stations = pd.read_csv(
+    "/Users/vincentcheung/Desktop/Coding/Python-1/2_Intermediate_Project/app6_build_weather_api/additional/data_small/stations.txt",
+    skiprows=18,
+    names=station_header,
+    index_col=False,
+)
+cleanstation = stations[["id", "station_name", "cn"]]
+
 
 @app.route("/")
 def home():
-    return render_template("home.html")
+    return render_template("home.html", data=cleanstation.to_html())
 
 
 @app.route("/api/v1/<station>/<date>")
 def about(station, date):
     station_id = str(station).zfill(6)
-    print(station_id)
     weather_df_header = ["station_id", "sou_id", "date", "temp", "q_tg"]
     filename = f"/Users/vincentcheung/Desktop/Coding/Python-1/2_Intermediate_Project/app6_build_weather_api/additional/data_small/TG_STAID{station_id}.txt"
+
     df = pd.read_csv(
-        filename,
-        skiprows=21,
-        sep=",",
-        names=weather_df_header,
-        parse_dates=["date"],
+        filename, skiprows=21, sep=",", names=weather_df_header, parse_dates=["date"]
     )
     temperature = df.loc[df["date"] == date]["temp"].squeeze() / 10
-    return {"station": station_id, "date": date, "temperature": temperature}
+
+    return {
+        "station": station_id,
+        "date": date,
+        "temperature": temperature,
+    }
+
+
+@app.route("/api/v1/<station>")
+def get_all_station_date(station):
+    station_id = str(station).zfill(6)
+    weather_df_header = ["station_id", "sou_id", "date", "temp", "q_tg"]
+    filename = f"/Users/vincentcheung/Desktop/Coding/Python-1/2_Intermediate_Project/app6_build_weather_api/additional/data_small/TG_STAID{station_id}.txt"
+
+    df = pd.read_csv(
+        filename, skiprows=21, sep=",", names=weather_df_header, parse_dates=["date"]
+    )
+
+    result = df.to_dict(orient="records")
+    return result
 
 
 @app.route("/api/v1/translator/<word>")
