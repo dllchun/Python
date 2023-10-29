b42bbbf53373aa703a06ed78362ab39e4be0ba84
dllchun <wingchuncheung0108@gmail.com>
2023-10-29 18:02:06 +0800

    day 36 end

--- 2_Intermediate_Project/app9_webcam/emailing.py
+++ 2_Intermediate_Project/app9_webcam/emailing.py
@@ -2,14 +2,15 @@ import smtplib
 
 
 def send_email():
-    username = ""
-    password = ""
+    # username = ""
+    # password = ""
 
-    host = "smpt.google.com"
-    port = 465
-    message = ""
-    receiver = ""
+    # host = "smpt.google.com"
+    # port = 465
+    # message = ""
+    # receiver = ""
 
-    with smtplib.SMTP_SSL(host, port) as server:
-        server.login(username, password)
-        server.sendmail(username, receiver, message)
+    # with smtplib.SMTP_SSL(host, port) as server:
+    #     server.login(username, password)
+    #     server.sendmail(username, receiver, message)
+    print("email was send")
--- 2_Intermediate_Project/app9_webcam/main.py
+++ 2_Intermediate_Project/app9_webcam/main.py
@@ -1,18 +1,56 @@
 import cv2
 import numpy as np
+import time
+from emailing import send_email
 
-# array = cv2.imread("2_Intermediate_Project/app9_webcam/image.png")
+video = cv2.VideoCapture(1)
+time.sleep(1)
 
-# # print(array.shape)
-# print(type(array))
+first_frame = None
+status_list = []
 
-a = np.array(
-    [
-        [[255, 0, 0], [255, 255, 255], [255, 255, 255], [187, 41, 160]],
-        [[255, 255, 255], [255, 255, 255], [255, 255, 255], [255, 255, 255]],
-        [[255, 255, 255], [0, 0, 0], [47, 255, 173], [255, 255, 255]],
-    ]
-)
+while True:
+    status = 0
+    check, frame = video.read()
+    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)  # set to gray color
+    gray_frame_gau = cv2.GaussianBlur(gray_frame, (21, 21), 0)
+
+    if first_frame is None:
+        first_frame = gray_frame_gau
+
+    delta_frame = cv2.absdiff(first_frame, gray_frame_gau)
+
+    thresh_frame = cv2.threshold(delta_frame, 60, 255, cv2.THRESH_BINARY)[1]
+    dil_frame = cv2.dilate(thresh_frame, None, iterations=2)
+
+    # 獲得輪廓
+    contours, check = cv2.findContours(
+        dil_frame, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE
+    )
+
+    for contour in contours:
+        if cv2.contourArea(contour) < 5000:
+            continue
+        x, y, w, h = cv2.boundingRect(contour)
+        rectangle = cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 3)
+
+        if rectangle.any():
+            status = 1
+
+    # Use status_list to track the last two output to see if it match [1,0] << disapear
+    status_list.append(status)
+    status_list = status_list[-2:]
+    if status_list[0] == 1 and status_list[1] == 0:
+        send_email()
+
+    print(status_list)
+
+    cv2.imshow("Video", frame)
+
+    key = cv2.waitKey(1)
+    # when user press Q, it will terminate the programme
+    if key == ord("q"):
+        break
 
 
-cv2.imwrite("2_Intermediate_Project/app9_webcam/image2.png", a)
+video.release()
