6bca49c67ab37e86f53a94e1e740d5d85f8af0ad
dllchun <wingchuncheung0108@gmail.com>
2023-11-05 15:33:12 +0800

    day 40 end (OOP start)

--- 2_Intermediate_Project/app10_musiccrawler/main.py
+++ 2_Intermediate_Project/app10_musiccrawler/main.py
@@ -4,7 +4,6 @@ from emailing import send_email
 import time
 import os.path
 import sqlite3
-import logging
 
 
 URL = "https://programmer100.pythonanywhere.com/tours/"
@@ -12,60 +11,63 @@ HEADERS = {
     "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36"
 }
 
-# db setting
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-db_path = os.path.join(BASE_DIR, "data.db")
 
-with sqlite3.connect(db_path) as db:
-    cursor = db.cursor()
+class Events:
+    def scraper(self, url):
+        """Scrpae the page source from the URL"""
+        response = requests.get(url, headers=HEADERS)
+        source = response.text
+        return source
 
-
-def scraper(url):
-    """Scrpae the page source from the URL"""
-    response = requests.get(url, headers=HEADERS)
-    source = response.text
-    return source
-
-
-def extract(source):
-    extractor = selectorlib.Extractor.from_yaml_file(
-        "2_Intermediate_Project/app10_musiccrawler/extract.yaml"
-    )
-    value = extractor.extract(source)["tours"]
-    return value
+    def extract(self, source):
+        extractor = selectorlib.Extractor.from_yaml_file(
+            "2_Intermediate_Project/app10_musiccrawler/extract.yaml"
+        )
+        value = extractor.extract(source)["tours"]
+        return value
 
 
-def stored_extracted(extracted):
-    row = extracted.split(",")
-    row = [row.strip() for row in row]
-    band, city, date = row
-    cursor.execute("INSERT INTO events VALUES(?,?,?)", row)
-    db.commit()
+class Database:
+    def __init__(self, database_path):
+        print("Database Connected")
+        BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+        db_path = os.path.join(BASE_DIR, database_path)
 
+        self.connection = sqlite3.connect(db_path)
+        print(self.connection)
 
-def read_extracted(extracted):
-    row = extracted.split(",")
-    row = [row.strip() for row in row]
-    band, city, date = row
-    cursor.execute(
-        "SELECT * FROM events WHERE band=? AND city=? AND date =?", (band, city, date)
-    )
-    rows = cursor.fetchall()
-    return rows
+    def stored(self, extracted):
+        row = extracted.split(",")
+        row = [row.strip() for row in row]
+        band, city, date = row
+        self.cursor = self.connection.cursor()
+        self.cursor.execute("INSERT INTO events VALUES(?,?,?)", row)
+        self.connection.commit()
 
-
-# def insert_to_db():
+    def read(self, extracted):
+        row = extracted.split(",")
+        row = [row.strip() for row in row]
+        band, city, date = row
+        self.cursor = self.connection.cursor()
+        self.cursor.execute(
+            "SELECT * FROM events WHERE band=? AND city=? AND date =?",
+            (band, city, date),
+        )
+        rows = self.cursor.fetchall()
+        return rows
 
 
 if __name__ == "__main__":
     while True:
-        source = scraper(URL)
-        extracted = extract(source)
+        event = Events()
+        source = event.scraper(URL)
+        extracted = event.extract(source)
         print(extracted)
 
         if extracted != "No upcoming tours":
-            row = read_extracted(extracted)
+            database = Database(database_path="data.db")
+            row = database.read(extracted)
             if not row:
-                stored_extracted(extracted)
+                database.stored(extracted)
                 send_email(message=f"New event was found \n {extracted}")
         time.sleep(2)
