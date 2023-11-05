57b1a42a77a9b4bda268e0beaa532624bcbd6785
dllchun <wingchuncheung0108@gmail.com>
2023-11-05 13:46:53 +0800

    day 39 additional task

--- 2_Intermediate_Project/app10_musiccrawler/additional/main.py
+++ 2_Intermediate_Project/app10_musiccrawler/additional/main.py
@@ -2,21 +2,24 @@ import requests
 from bs4 import BeautifulSoup
 from datetime import datetime
 import time
+import sqlite3
 
 URL = "https://programmer100.pythonanywhere.com/"
 HEADERS = {
     "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36"
 }
 
+db_path = "2_Intermediate_Project/app10_musiccrawler/data.db"
+
+with sqlite3.connect(db_path) as conn:
+    cursor = conn.cursor()
+
 
 def get_html(url):
     # Request the html
     r = requests.get(url)
     content = r.text
-    return content
 
-
-def get_temperature_data(content):
     # Process the html
     soup = BeautifulSoup(content, "html.parser")
     temperature = soup.find("h1", id="temperatureId")
@@ -25,9 +28,9 @@ def get_temperature_data(content):
 
 
 def store_temperature(temperature):
-    with open("file.txt", "a") as file:
-        time = datetime.now()
-        file.write(f"{time}, {temperature}\n")
+    time = datetime.now().strftime("%y-%m-%d-%H-%M-%S")
+    cursor.execute("INSERT INTO temperature VALUES(?,?)", (time, temperature))
+    conn.commit()
 
 
 def get_temperature(url):
@@ -36,3 +39,10 @@ def get_temperature(url):
         temperature = get_temperature_data(content)
         store_temperature(temperature)
         time.sleep(30)
+
+
+if __name__ == "__main__":
+    while True:
+        temperature = get_html(URL)
+        store_temperature(temperature)
+        time.sleep(5)
