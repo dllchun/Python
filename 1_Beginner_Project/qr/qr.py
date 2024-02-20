3382cf3ca7728807d3d5757766e886be4fc6d50b
dllchun <wingchuncheung0108@gmail.com>
2024-02-20 13:55:57 +0800

    sync

--- 1_Beginner_Project/qr/qr.py
+++
@@ -1,9 +0,0 @@
-import qrcode
-
-data = 'Fuck you'
-qr = qrcode.QRCode(version=1, box_size=10, border=5)
-qr.add_data(data)
-qr.make(fit=True)
-
-img=qr.make_image()
-img.save('/Users/vincentcheung/Desktop/Coding/Python/Beginner-Project/qr/qr1.png')
--- 2_Intermediate_Project/app8_nlp_books/.ipynb_checkpoints/main-checkpoint.py
+++
@@ -1,6 +0,0 @@
-with open(
-    "2_Intermediate_Project/app8_nlp_books/miracle_in_the_andes.txt",
-    "r",
-) as book:
-    content = book.read()
-    print(content)
